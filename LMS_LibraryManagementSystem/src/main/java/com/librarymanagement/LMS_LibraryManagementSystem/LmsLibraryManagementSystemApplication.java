cf913575dc9327c15ceeafc6eb2f86296e702780
Sahil <cseit18sdjaiswaljd@gmail.com>
2022-05-28 01:05:40 +0530

    First LMS

---
+++ LMS_LibraryManagementSystem/src/main/java/com/librarymanagement/LMS_LibraryManagementSystem/LmsLibraryManagementSystemApplication.java
@@ -0,0 +1,13 @@
+package com.librarymanagement.LMS_LibraryManagementSystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LmsLibraryManagementSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LmsLibraryManagementSystemApplication.class, args);
+	}
+
+}
---
+++ LMS_LibraryManagementSystem/src/test/java/com/librarymanagement/LMS_LibraryManagementSystem/LmsLibraryManagementSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.librarymanagement.LMS_LibraryManagementSystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LmsLibraryManagementSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
